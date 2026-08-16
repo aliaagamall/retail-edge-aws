b45b9b02abc0fce3d559550d1d59ab4bd8a31b02
Aliaa Gamal <alliaagamall@gmail.com>
2026-08-17 01:15:22 +0300

    chore: bootstrap terraform backend

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Short project prefix used in resource names/tags"
+  type        = string
+  default     = "retailedge"
+}
+
+variable "environment" {
+  description = "Deployment environment (prod, staging, dev)"
+  type        = string
+  default     = "dev"
+}
