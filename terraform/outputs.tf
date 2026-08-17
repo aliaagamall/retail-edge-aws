25cf86864df437cc3b6b2e807cbe27b7936a9362
Aliaa Gamal <aliaa@gmail.com>
2026-08-17 13:55:02 +0300

    feat(security): add tiered security groups

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,4 @@
+# Network outputs
 output "vpc_id" {
   value = module.networking.vpc_id
 }
@@ -12,4 +13,25 @@ output "app_subnet_ids" {
 
 output "db_subnet_ids" {
   value = module.networking.db_subnet_ids
+}
+
+# Security groups outputs
+output "alb_sg_id" {
+  value = module.security_groups.alb_sg_id
+}
+
+output "app_sg_id" {
+  value = module.security_groups.app_sg_id
+}
+
+output "rds_sg_id" {
+  value = module.security_groups.rds_sg_id
+}
+
+output "redis_sg_id" {
+  value = module.security_groups.redis_sg_id
+}
+
+output "endpoints_sg_id" {
+  value = module.security_groups.endpoints_sg_id
 }
