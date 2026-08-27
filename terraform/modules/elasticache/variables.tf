bcaf9c53319c98cbdc0e78ba96fbf2bc3c726dd4
aliaa😜 <alliaagamall@gmail.com>
2026-08-27 23:40:39 +0300

    refactor: parameterize Redis module settings

--- terraform/modules/elasticache/variables.tf
+++ terraform/modules/elasticache/variables.tf
@@ -7,7 +7,7 @@ variable "environment" {
 }
 
 variable "db_subnet_ids" {
-  description = "Using the same private database subnets as RDS"
+  description = "Private database subnet IDs used by Redis"
   type        = list(string)
 }
 
@@ -16,12 +16,43 @@ variable "redis_sg_id" {
 }
 
 variable "node_type" {
-  type    = string
-  default = "cache.t3.micro"
+  description = "ElastiCache Redis node type"
+  type        = string
+  default     = "cache.t3.micro"
+}
+
+variable "engine_version" {
+  description = "Redis engine version"
+  type        = string
+  default     = "7.1"
+}
+
+variable "num_cache_clusters" {
+  description = "Number of cache nodes in the replication group"
+  type        = number
+  default     = 2
+}
+
+variable "automatic_failover_enabled" {
+  description = "Enable automatic failover between primary and replica"
+  type        = bool
+  default     = true
+}
+
+variable "multi_az_enabled" {
+  description = "Enable Multi-AZ for Redis"
+  type        = bool
+  default     = true
 }
 
 variable "auth_enabled" {
   description = "Enable Redis AUTH token"
   type        = bool
   default     = true
+}
+
+variable "auth_token_length" {
+  description = "Length of the generated Redis AUTH token"
+  type        = number
+  default     = 32
 }
