4f57d43ddb2be4d9592c2456087214251978b11c
aliaa😜 <alliaagamall@gmail.com>
2026-08-27 23:31:48 +0300

    refactor: parameterize RDS environment settings

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -48,19 +48,76 @@ variable "waf_rate_limit" {
 
 # ---------- RDS ----------
 
-variable "rds_skip_final_snapshot" {
-  type    = bool
-  default = true
+variable "rds_db_name" {
+  description = "Initial RDS database name"
+  type        = string
+  default     = "retailedge"
 }
 
-variable "rds_backup_retention_period" {
-  type    = number
-  default = 0
+variable "rds_db_username" {
+  description = "RDS master username"
+  type        = string
+  default     = "retailedge_admin"
 }
 
 variable "rds_instance_class" {
-  type    = string
-  default = "db.t3.micro"
+  description = "RDS instance class"
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "rds_allocated_storage" {
+  description = "RDS allocated storage in GB"
+  type        = number
+  default     = 20
+}
+
+variable "rds_engine_version" {
+  description = "MySQL engine version"
+  type        = string
+  default     = "8.0"
+}
+
+variable "rds_password_length" {
+  description = "Length of the generated RDS password"
+  type        = number
+  default     = 20
+}
+
+variable "rds_backup_retention_period" {
+  description = "Automated backup retention period in days"
+  type        = number
+  default     = 0
+}
+
+variable "rds_backup_window" {
+  description = "Preferred daily RDS backup window"
+  type        = string
+  default     = "03:00-04:00"
+}
+
+variable "rds_maintenance_window" {
+  description = "Preferred weekly RDS maintenance window"
+  type        = string
+  default     = "mon:04:30-mon:05:30"
+}
+
+variable "rds_multi_az" {
+  description = "Enable RDS Multi-AZ deployment"
+  type        = bool
+  default     = true
+}
+
+variable "rds_skip_final_snapshot" {
+  description = "Skip final snapshot when destroying RDS"
+  type        = bool
+  default     = true
+}
+
+variable "rds_deletion_protection" {
+  description = "Protect RDS from accidental deletion"
+  type        = bool
+  default     = false
 }
 
 # ---------- ElastiCache ----------
