61199f76c7599d83c56a7f27e0f5666bb7a2cd0d
aliaa😜 <alliaagamall@gmail.com>
2026-08-27 23:28:13 +0300

    refactor: parameterize RDS module settings

--- terraform/modules/rds/variables.tf
+++ terraform/modules/rds/variables.tf
@@ -15,24 +15,39 @@ variable "rds_sg_id" {
 }
 
 variable "db_name" {
-  description = "Initial database name"
+  description = "Database name"
   type        = string
   default     = "retailedge"
 }
 
 variable "db_username" {
-  type    = string
-  default = "retailedge_admin"
+  description = "Master database username"
+  type        = string
+  default     = "retailedge_admin"
 }
 
 variable "instance_class" {
-  type    = string
-  default = "db.t3.micro"
+  description = "RDS instance class"
+  type        = string
+  default     = "db.t3.micro"
 }
 
 variable "allocated_storage" {
-  type    = number
-  default = 20
+  description = "Allocated RDS storage in GB"
+  type        = number
+  default     = 20
+}
+
+variable "engine_version" {
+  description = "MySQL engine version"
+  type        = string
+  default     = "8.0"
+}
+
+variable "password_length" {
+  description = "Length of the generated database password"
+  type        = number
+  default     = 20
 }
 
 variable "backup_retention_period" {
@@ -41,13 +56,32 @@ variable "backup_retention_period" {
   default     = 0
 }
 
+variable "backup_window" {
+  description = "Preferred daily backup window"
+  type        = string
+  default     = "03:00-04:00"
+}
+
+variable "maintenance_window" {
+  description = "Preferred weekly maintenance window"
+  type        = string
+  default     = "mon:04:30-mon:05:30"
+}
+
 variable "multi_az" {
-  type    = bool
-  default = true
+  description = "Enable Multi-AZ deployment"
+  type        = bool
+  default     = true
 }
 
 variable "skip_final_snapshot" {
-  description = "Set true only for throwaway/dev environments"
+  description = "Skip final snapshot when destroying the database"
+  type        = bool
+  default     = true
+}
+
+variable "deletion_protection" {
+  description = "Protect the database from accidental deletion"
   type        = bool
   default     = false
 }
