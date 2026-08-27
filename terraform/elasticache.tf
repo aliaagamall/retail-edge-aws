2101a55f999e62bdba54b8d0da74e6ae7430745c
aliaa😜 <alliaagamall@gmail.com>
2026-08-27 23:41:58 +0300

    refactor: pass environment-specific Redis settings

--- terraform/elasticache.tf
+++ terraform/elasticache.tf
@@ -5,5 +5,12 @@ module "elasticache" {
   environment   = var.environment
   db_subnet_ids = module.networking.db_subnet_ids
   redis_sg_id   = module.security_groups.redis_sg_id
-  node_type     = var.redis_node_type   
+
+  node_type                 = var.redis_node_type
+  engine_version             = var.redis_engine_version
+  num_cache_clusters         = var.redis_num_cache_clusters
+  automatic_failover_enabled = var.redis_automatic_failover_enabled
+  multi_az_enabled            = var.redis_multi_az_enabled
+  auth_enabled               = var.redis_auth_enabled
+  auth_token_length          = var.redis_auth_token_length
 }
