24a5d7fd800268dc87300854543bd66ac0dbca4b
aliaa😜 <alliaagamall@gmail.com>
2026-08-27 22:45:18 +0300

    fix(s3-web): allow CloudFront OAC access

--- terraform/s3-web.tf
+++ terraform/s3-web.tf
@@ -3,5 +3,37 @@ module "s3_web" {
 
   project_name = var.project_name
   environment  = var.environment
-  cloudfront_distribution_arn = module.cloudfront.distribution_arn
+}
+
+data "aws_iam_policy_document" "cloudfront_s3_access" {
+  statement {
+    sid    = "AllowCloudFrontServicePrincipalReadOnly"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${module.s3_web.bucket_arn}/*"
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values = [
+        module.cloudfront.distribution_arn
+      ]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "web" {
+  bucket = module.s3_web.bucket_name
+  policy = data.aws_iam_policy_document.cloudfront_s3_access.json
 }
