946568bc1a7e2e66468dbe45716d49bcbbabc5ed
aliaa😜 <alliaagamall@gmail.com>
2026-08-27 22:43:28 +0300

    refactor(s3-web): remove unused CloudFront variable

--- terraform/modules/s3-web/variables.tf
+++ terraform/modules/s3-web/variables.tf
@@ -5,9 +5,3 @@ variable "project_name" {
 variable "environment" {
   type = string
 }
-
-variable "cloudfront_distribution_arn" {
-  description = "ARN of the CloudFront distribution allowed to read this bucket (i w'll set it after CloudFront is created)"
-  type        = string
-  default     = ""
-}
