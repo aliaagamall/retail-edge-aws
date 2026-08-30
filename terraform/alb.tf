284a6811baeaa76f06aed7e6724c038042fdf788
aliaa😜 <alliaagamall@gmail.com>
2026-08-31 02:40:45 +0300

    Add certificate_arn to ALB module configuration
    Added certificate_arn variable to ALB module.

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -6,5 +6,6 @@ module "alb" {
   vpc_id                     = module.networking.vpc_id
   app_subnet_ids             = module.networking.app_subnet_ids
   alb_sg_id                  = module.security_groups.alb_sg_id
+  certificate_arn            = var.certificate_arn
   enable_deletion_protection = var.alb_deletion_protection
 }
