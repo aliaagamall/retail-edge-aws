bde78ff9a2b73de0bf95441d7144a12f39f55800
aliaa😜 <alliaagamall@gmail.com>
2026-08-31 18:52:59 +0300

    Add secret recovery window configuration to elasticache
    Added secret_recovery_window_in_days variable to elasticache module.

--- terraform/elasticache.tf
+++ terraform/elasticache.tf
@@ -6,6 +6,8 @@ module "elasticache" {
   db_subnet_ids = module.networking.db_subnet_ids
   redis_sg_id   = module.security_groups.redis_sg_id
 
+secret_recovery_window_in_days = var.secret_recovery_window_in_days
+
   node_type                  = var.redis_node_type
   engine_version             = var.redis_engine_version
   num_cache_clusters         = var.redis_num_cache_clusters
